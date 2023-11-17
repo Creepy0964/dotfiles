983c1b65b62bd14280fe05411d58bf4d9d89f7a4
Creepy0964 <59210160+Creepy0964@users.noreply.github.com>
2023-11-17 22:17:52 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
-# dotfiles
-All my i3-gaps config files.
+# Creepy0964's dotfiles
+All my i3wm config files.
+
+## Prerequisites
+- `i3wm`
+- `polybar`
+- `picom`
+- `alacritty`
+- `feh`
+- `i3lock`
+- `rofi`
+
+## Installation
+For `Arch Linux` and `Arch-based` distributions, there's a script named `install_i3wm.sh`.
+
+### Script installation
+```bash
+git clone https://github.com/creepy0964/dotfiles dir-name
+cd dir-name
+chmod +x install_i3wm.sh
+./install_i3wm.sh
+```
+If you encountered any errors, feel free to create an issue and refer to manual installation.
+
+### Manual installation
+```bash
+git clone https://github.com/creepy0964/dotfiles dir-name
+cd dir-name
+mkdir ~/.config/i3 && cp ./config/i3/config ~/.config/i3/config
+mkdir ~/.config/polybar && cp ./config/polybar/config.ini ~/.config/polybar/config.ini
+mkdir ~/.config/picom && cp ./config/picom/picom.conf ~/.config/picom/picom.conf
+cp ./config/polybar/launch_polybar.sh ~/.config/polybar/launch_polybar.sh
+chmod +x ~/.config/polybar/launch_polybar.sh
+cp ./images/wallpaper3.png ~/Pictures/wallpaper3.png
+```
+If you encountered any problems, feel free to create an issue or contact me in [telegram](https://t.me/notcreepy0964).
+
+## Preview
+![preview](dotfiles_preview.png)
+Note: for `polybar` in my installation I am using custom font named Uni Neue Regular. Will be added in future revisions of `dotfiles`.
--- install_i3wm.sh
+++ install_i3wm.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 clear
 
+workdir = "~/creepy0964-dotfiles"
+i3 = "~/.config/i3"
+polybar = "~/.config/polybar"
+picom = "~/.config/picom"
+
 if [ "$EUID" -eq 0 ]; then
     echo -e 'Do NOT execute this script as sudo/root.'
     exit
@@ -19,14 +24,30 @@ fi
 
 while pgrep -x rm > /dev/null; do sleep 1; done
 
-git clone https://github.com/creepy0964/dotfiles ~/creepy0964-dotfiles
+git clone https://github.com/creepy0964/dotfiles $workdir
 
 while pgrep -x git > /dev/null; do sleep 1; done
 
-mkdir -p ~/.config/i3 && cp creepy0964-dotfiles/i3/config ~/.config/i3/config
-mkdir -p ~/.config/polybar && cp creepy0964-dotfiles/polybar/config.ini ~/.config/polybar/config.ini
-cp creepy0964-dotfiles/images/wallpaper3.png ~/Pictures
-cp creepy0964-dotfiles/launch_polybar.sh ~/launch_polybar.sh
-chmod +x ~/launch_polybar.sh
+if ! [ -d ~/.config/i3 ]; then
+    echo -e '\nNo i3 directory. Creating...'
+    mkdir -p ~/.config/i3
+fi
+
+if ! [ -d ~/.config/polybar ]; then
+    echo -e '\nNo polybar directory. Creating...'
+    mkdir -p ~/.config/polybar
+fi
+
+if ! [ -d ~/.config/picom ]; then
+    echo -e '\nNo picom directory. Creating...'
+    mkdir -p ~/.config/picom
+fi
+
+cp $workdir/config/i3/config $i3/config
+cp $workdir/config/polybar/config.ini $polybar/config.ini
+cp $workdir/config/picom/picom.conf $picom/picom.conf
+cp $workdir/images/wallpaper3.png ~/Pictures/wallpaper3.png
+cp $workdir/launch_polybar.sh $polybar/launch_polybar.sh
+chmod +x $polybar/launch_polybar.sh
 
 echo -e '\n\n\nDone. Now you can log out from your current DE/WM and log in into i3-wm.\n'
